e482496f60e8cb0808e8961d837eb2d96d4c9b57
Abrar Zawad <abrarzawad222@gmail.com>
2024-05-27 22:38:59 +0600

    parallax

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,24 +1,32 @@
+import { Parallax } from "react-parallax";
+
 const Cover = ({ img, title, subtitle }) => {
   return (
-    <div
-      className="hero min-h-[550px]"
-      style={{
-        backgroundImage: `url("${img}")`,
-      }}
+    <Parallax
+      blur={{ min: -50, max: 50 }}
+      bgImage={img}
+      bgImageAlt="the dog"
+      strength={-200}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div
-        className="hero-content text-center text-neutral-content ff mt-10"
-        style={{
-          background: 'rgba(21, 21, 21, 0.60)'
-        }}
-      >
-        <div className="py-[80px] px-[300px] text-center">
-          <h1 className="mb-5 text-[70px] font-extrabold uppercase">{title}</h1>
-          <p className="mb-5 text-xl uppercase font-semibold mx-auto">{subtitle}</p>
+      <div className="hero min-h-[550px]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div
+          className="hero-content text-center text-neutral-content ff mt-10"
+          style={{
+            background: "rgba(21, 21, 21, 0.60)",
+          }}
+        >
+          <div className="py-[80px] px-[300px] text-center">
+            <h1 className="mb-5 text-[70px] font-extrabold uppercase">
+              {title}
+            </h1>
+            <p className="mb-5 text-xl uppercase font-semibold mx-auto">
+              {subtitle}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
