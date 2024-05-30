a654ca308e8bec8e1708970a71b819f1704e57f3
Abrar Zawad <abrarzawad222@gmail.com>
2024-05-30 15:03:15 +0600

    doing order page

---
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -0,0 +1,24 @@
+
+
+const FoodCard = ({item}) => {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={item.image}
+          alt="Shoes"
+        />
+      </figure>
+      <p className=" absolute right-0 mr-3 mt-3 py-1 px-2 bg-black text-white font-medium text-base">${item.price}</p>
+      <div className="card-body">
+        <h2 className="text-center text-[#151515] font-semibold text-[24px]">{item.name}</h2>
+        <p className="text-center text-[#737373] text-base pb-8">{item.recipe}</p>
+        <div className="card-action text-center">
+          <button className="border-b-[3px] border-[#BB8506] rounded-lg p-[20px] text-[#BB8506] bg-[#E8E8E8] px-8 py-5 uppercase active:bg-[#111827]">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,13 +1,60 @@
 import Cover from "../../Shared/Cover/Cover";
-import cover from  "../../../assets/shop/banner2.jpg"
+import cover from "../../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
 
 const Order = () => {
-    return (
-        <div>
-            <Cover img={cover} title={"Our Shop"} subtitle={"Would you like to buy a dish"}></Cover>
-            
-        </div>
-    );
+  const [tindex, setIndex] = useState(0);
+
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const drink = menu.filter((item) => item.category === "drinks");
+
+  return (
+    <div>
+      <Cover
+        img={cover}
+        title={"Our Shop"}
+        subtitle={"Would you like to buy a dish"}
+      ></Cover>
+      <div className="max-w-5xl mx-auto">
+        <Tabs
+          defaultIndex={tindex}
+          onSelect={(index) => setIndex(index)}
+          className="text-center mt-[50px]"
+        >
+          <TabList>
+            <Tab>Salad</Tab>
+            <Tab>Pizza</Tab>
+            <Tab>Soups</Tab>
+            <Tab>Desserts</Tab>
+            <Tab>Drinks</Tab>
+          </TabList>
+          <TabPanel>
+            <OrderTab items={salad}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+          <OrderTab items={pizza}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+          <OrderTab items={soup}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+          <OrderTab items={dessert}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+          <OrderTab items={drink}></OrderTab>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
