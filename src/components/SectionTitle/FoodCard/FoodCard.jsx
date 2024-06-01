254707bdeaef8b113b0f73f06ceb67ab21bfa6a4
Abrar Zawad <abrarzawad222@gmail.com>
2024-06-01 17:07:08 +0600

    connected server

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -14,7 +14,7 @@ const FoodCard = ({item}) => {
         <h2 className="text-center text-[#151515] font-semibold text-[24px]">{item.name}</h2>
         <p className="text-center text-[#737373] text-base pb-8">{item.recipe}</p>
         <div className="card-action text-center">
-          <button className="border-b-[3px] border-[#BB8506] rounded-lg p-[20px] text-[#BB8506] bg-[#E8E8E8] px-8 py-5 uppercase active:bg-[#111827]">Add to Cart</button>
+          <button className="border-b-[3px] border-[#BB8506] rounded-lg p-[20px] text-[#BB8506] bg-[#E8E8E8] px-8 py-5 uppercase hover:bg-[#111827]">Add to Cart</button>
         </div>
       </div>
     </div>
